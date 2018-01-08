ad0f633adf5c01ef6073892cf5dec75546e6df94
A Lee <alee@fwparker.org>
2018-01-08 16:08:30 -0600

    Add files via upload

---
+++ Matplotlib/power_problem.py
@@ -0,0 +1,34 @@
+'''
+Use the power_data.csv file AND the zipcode database
+to answer the questions below.  Make sure all answers
+are printed in a readable format. (i.e. "The city with the highest electricity cost in Illinois is XXXXX."
+
+The power_data dataset, compiled by NREL using data from ABB,
+the Velocity Suite and the U.S. Energy Information
+Administration dataset 861, provides average residential,
+commercial and industrial electricity rates by zip code for
+both investor owned utilities (IOU) and non-investor owned
+utilities. Note: the file includes average rates for each
+utility, but not the detailed rate structure data found in the
+OpenEI U.S. Utility Rate Database.
+
+This is a big dataset.
+Below are some questions that you likely would not be able
+to answer without some help from a programming language.
+It's good geeky fun.  Enjoy
+
+FOR ALL THE RATES, ONLY USE THE BUNDLED VALUES (NOT DELIVERY).  This rate includes transmission fees and grid fees that are part of the true rate.
+'''
+
+#1  What is the average residential rate for YOUR zipcode? You will need to read the power_data into your program to answer this.  (7pts)
+
+#2 What is the MEDIAN rate for all BUNDLED RESIDENTIAL rates in Illinois? Use the data you extracted to check all "IL" zipcodes to answer this. (10pts)
+
+#3 What city in Illinois has the lowest residential rate?  Which has the highest?  You will need to go through the database and compare each value for this one. Then you will need to reference the zipcode dataset to get the city.  (15pts)
+
+
+#FOR #4  CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS. The first one is easier than the second.
+#4  (Easier) USING ONLY THE ZIP CODE DATA... Make a scatterplot of all the zip codes in Illinois according to their Lat/Long.  Make the marker size vary depending on the population contained in that zip code.  Add an alpha value to the marker so that you can see overlapping markers.
+
+#4 (Harder) USING BOTH THE ZIP CODE DATA AND THE POWER DATA... Make a scatterplot of all zip codes in Illinois according to their Lat/Long.  Make the marker red for the top 25% in residential power rate.  Make the marker yellow for the middle 25 to 50 percentile. Make the marker green if customers pay a rate in the bottom 50% of residential power cost.  This one is very challenging.  You are using data from two different datasets and merging them into one.  There are many ways to solve. (20pts)
+
