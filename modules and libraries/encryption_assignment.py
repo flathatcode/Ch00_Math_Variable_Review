5efaee899080715c90e213716f52d55315a5f5bb
A Lee <alee@fwparker.org>
2018-01-08 14:32:28 -0600

    Create encryption_assignment.py

---
+++ modules and libraries/encryption_assignment.py
@@ -0,0 +1,12 @@
+# MODULES AND LIBRARIES
+'''
+For this assignment, we will practice the use of imports to encrypt and decrypt messages.
+The functions are already contained in the files.  Your job is to use them to encrypt and decrypt strings.  Good luck
+'''
+
+#1 Decrypt this message using imports from the decode.py and encryption_key.py.  Make the result print in a friendly format that is easy for the user to understand. (10pt)
+encrypted_message = "¿®ªªÈÙ®ÏT¤ÕEÓ¹âeCíÉÁÏº¢¡i¸ºÇ¿"
+
+#2 Encrypt your name and print the encrypted result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
+
+#3 Decrypt the encrypted code from part 2 to ensure that it worked properly and print the result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
