f1feb822391c8cf4764742121a0f9dc6c1c714e1
A Lee <alee@fwparker.org>
2018-01-22 01:50:39 -0600

    Update function_problems.py

--- function_problems.py
+++ function_problems.py
@@ -5,19 +5,16 @@
 # Make a call to that function
 
 # PROBLEM 2 (Biggest, smallest, average - 4pts)
-# Make a function to ask the user to enter three numbers.
-# Then print the largest, the smallest, and their average, rounded to 2 decimals.
+# Make a function which takes 3 numbers as parameters.
+# The function then prints the largest, the smallest, and their average, rounded to 2 decimals.
 # Display the answers in a "nicely" formatted way.
 # Make a call to your function.
 
-# PROBLEM 3 (e to the... - 3pts)
-# Calculate the value of e (from the math library) to the power of -1, 0, 1, 2, and 3.
-# display the results, with 5 decimals, in a nicely formatted manner.
+# PROBLEM 3 (Random Numbers - 4pts)
+# Use the random() and randrange() functions to perform the following
+# Generate a random integer between -3 and 5 (-3 and 5 both included)
+# Generate a random float between 4 and 6.
 
-# PROBLEM 4 (Random Numbers - 4pts)
-# Generate a random integer between -3 and 5 (-3 and 5 both included, use random.randrange()).
-# Generate a random float between 4 and 6 (use random.random())
-
-# PROBLEM 5 (add me, multiply me - 3pts)
+# PROBLEM 4 (add me, multiply me - 3pts)
 # Make a function which takes in two integers and RETURNS their sum AND their product.
 # Make a call to the function and capture the product and sum as variables.
