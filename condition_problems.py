01cbe4fad2402d3e83d921c5f75c9e7f4e6c393b
A Lee <alee@fwparker.org>
2018-01-22 01:38:03 -0600

    Update condition_problems.py

--- condition_problems.py
+++ condition_problems.py
@@ -2,9 +2,8 @@
 
 # PROBLEM 1 (GPA - 4pts)
 # Grades are values between 0 and 100
-# We will translate grades to letters using:
-# http://www.collegeboard.com/html/academicTracker-howtoconvert.html
-# Make a variable for your percentage grade.
+# We will translate grades to letters.
+# 90-100: A, 80-90: B, 70-80: C, 60-70: D, <60 F. (a grade of exactly 90 would be considered an A)
 # Make a series of if/elif/else statements to print the letter grade.
 # If the user enters a grade lower than zero or higher than 100, just give an error message.
 # Don't worry about making an exception for these right now.
@@ -12,22 +11,11 @@
 # PROBLEM 2 (Vowels - 5pts)
 # Ask the user to supply a string.
 # Print how many different vowels there are in the string.
-# The capital version of a lower case vowel is considered to be the same vowel.
-# y is not considered a vowel.
-# Try to print proper output (e.g., printing “There are 1 different vowels in the string” is ugly).
-# Example: When the user enters the string “It’s Owl Stretching Time,”
+# The capital version and lower case are considered the same vowel.
+# Produce a proper output (e.g., printing “There are 1 different vowels in the string” is ugly).
+# Example: When the user enters the string “It’s Owl Stretching Time.”
 # the program should say that there are 3 different vowels in the string.
 
-# PROBLEM 3 (Quadratic Equation - 6pts)
-# You can solve quadratic equations using the quadratic formula.
-# Quadratic equations are of the form Ax2 + Bx + C = 0.
-# Such equations have zero, one or two solutions.
-# The first solution is (−B + sqrt(B^22 − 4AC))/(2A).
-# The second solution is (−B − sqrt(B^2 − 4AC))/(2A).
-# There are no solutions if the value under the square root is negative.
-# There is one solution if the value under the square root is zero.
-# Write a program that asks the user for the values of A, B, and C,
-# then reports whether there are zero, one, or two solutions,
-# then prints those solutions.
-# Note: Make sure that you also take into account the case that A is zero,
-# and the case that both A and B are zero.
+# PROBLEM 3 (Login - 6pts)
+# Create a program which works according to the flow diagram PasswordFlowchart.png in this folder
+# Substitute your name for Rohan's, and use whatever generic password you want.
